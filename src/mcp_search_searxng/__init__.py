07fe8418e9ef6048ef199ca77e6888a6c086e491
Peter Reuterås <peter@reuteras.com>
2025-09-27 08:31:16 +0200

    Update configuration for checks and automation.

--- src/mcp_search_searxng/__init__.py
+++ src/mcp_search_searxng/__init__.py
@@ -1,5 +1,6 @@
 """Init file."""
 
+import asyncio
 import sys
 
 from mcp_search_searxng.server import search_searxng
@@ -7,6 +8,4 @@ from mcp_search_searxng.server import search_searxng
 
 def main() -> None:
     """Function not used."""
-    import asyncio
-
     print(asyncio.run(main=search_searxng(query=sys.argv[1])))
